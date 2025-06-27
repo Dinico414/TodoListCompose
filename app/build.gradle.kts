33e8bf6d9344f6e831fab48379bea225927534f3
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2025-06-27 13:04:17 +0200

    Upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 2
-        versionName = "0.2.0"
+        versionName = "0.2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
