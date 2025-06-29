34788a1b1e582808b497ef8640dd3bfc3577dcff
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2025-06-29 22:44:39 +0200

    upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.xenon.todolist"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
