f9d39399106801a52f316b607432bf69bb9fc021
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2025-08-01 18:38:34 +0200

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 4
-        versionName = "1.6.9"
+        versionName = "1.6.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
