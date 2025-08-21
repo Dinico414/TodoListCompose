3d9d3527d136d720976315493cb00fe5c9c912f7
Dinico414 <116182930+Dinico414@users.noreply.github.com>
2025-08-21 02:29:47 +0200

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 4
-        versionName = "1.7.4"
+        versionName = "1.7.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
